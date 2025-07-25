b56a52d2d04df59ede43fd4cfe50b50e1523257c
Eze Olea Figueroa <ezeoleaf@gmail.com>
2025-07-25 21:58:35 +0200

    Update tblogs.rb

--- Formula/tblogs.rb
+++ Formula/tblogs.rb
@@ -1,9 +1,9 @@
 class Tblogs < Formula
     desc "Terminal blog reader"
     homepage "https://github.com/ezeoleaf/tblogs"
-    url "https://github.com/ezeoleaf/tblogs/releases/download/v1.0.0/tblogs-darwin-amd64"
-    sha256 "b4af8482013076a5244c050f3a9edfa75dece02837d9a0e6ac822f257b9287f7"
-    version "1.0.0"
+    url "https://github.com/ezeoleaf/tblogs/releases/download/v1.1.0/tblogs-darwin-amd64"
+    sha256 "28156aa6a25f51055b12d05369949e10f1b568133b82e5ae9c4ac5e8ee4dc25b"
+    version "1.1.0"
   
     def install
       bin.install "tblogs-darwin-amd64" => "tblogs"
