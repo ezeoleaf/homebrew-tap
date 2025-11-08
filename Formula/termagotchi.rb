29b2164a79f5becd4684301acdab972101d74a30
Ezequiel Olea Figueroa <ezeoleaf@gmail.com>
2025-11-08 08:55:28 +0100

    update termagotchi

--- Formula/termagotchi.rb
+++ Formula/termagotchi.rb
@@ -1,9 +1,9 @@
 class Termagotchi < Formula
     desc "Terminal Tamagotchi"
     homepage "https://github.com/ezeoleaf/termagotchi"
-    url "https://github.com/ezeoleaf/termagotchi/releases/download/v0.1.0/termagotchi-darwin-amd64"
-    sha256 "44a3a88f0003e0b590a99b35733bc6942d0d80c57fefeac85031827e15513fc2"
-    version "0.1.0"
+    url "https://github.com/ezeoleaf/termagotchi/releases/download/v0.2.0/termagotchi-darwin-amd64"
+    sha256 "9aa962e5eb8554e22af01a8b20e289fb2bb97f5b46b3d48c48a1c1acea459b25"
+    version "0.2.0"
   
     def install
       bin.install "termagotchi-darwin-amd64" => "termagotchi"
